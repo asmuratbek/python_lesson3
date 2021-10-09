0001279c45d1c5d5c94537d289ae8374625be6e3
Muratbek Anvarbekov <asmuratbek.work@gmail.com>
2021-10-09 21:42:19 +0600

    first commit

---
+++ main.py
@@ -0,0 +1,38 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+# def print():
+#     name = "Murat"
+#     digit = 12
+#     list_of_digits = [1, 33, 4, 5, 5]
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    # name = "Murat"
+    # not_digit = "1"
+    # print(not_digit)
+    # print(type(not_digit))
+    # is_digit = int(not_digit)
+    # print(is_digit)
+    # print(type(is_digit))
+    # digit = 12
+    # list_of_digits = [1, 33, 4, 5, 5]
+    # str_list = str(list_of_digits)
+    # print(str_list)
+    # print(type(str_list))
+    # print(list_of_digits)
+    # print(type(list_of_digits))
+    # print(str(1)+str(1))
+    x = 1
+    x += 1
+    x = x + 1
+    x -= 1
+    x *= 2
+    x /= 2
+    print(x)
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
